e7e03d42bad36d63881ef603548725f99a730fae
Adam Badry <57472142+Sk3tch7y@users.noreply.github.com>
2024-03-10 17:47:41 -0700

    fixed conflict

--- Code/intercal/src/App.js
+++ Code/intercal/src/App.js
@@ -3,25 +3,18 @@ import './/styles/styles.css';
 import './/styles/app.css'
 import Sidebar from './/Sidebar.jsx';
 import Dashboard from './/dashboard.jsx';
-<<<<<<< Updated upstream
-=======
 import SearchBar from './SearchBar.jsx';
->>>>>>> Stashed changes
 
 function App() {
 
   return (
     <div className="App">
       <Sidebar></Sidebar>
-<<<<<<< Updated upstream
-      <Dashboard></Dashboard>
-=======
       <div className ='rightSide'>
         <SearchBar></SearchBar>
         <Dashboard></Dashboard>
       </div>
       
->>>>>>> Stashed changes
     </div>
   );
 }
