2c0442ba86cb86358c4c09545d3307db59786087
Adam Badry <57472142+Sk3tch7y@users.noreply.github.com>
2024-03-10 17:36:38 -0700

    Box sizing fixed
    Starting prototypes for post thumbnails. Will be waiting on Oakley to create statistics functions in order to apply them

---
+++ Code/intercal/src/SearchBar.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './/styles/searchbarStyles.css'
+const SearchBar = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSearch = (e) => {
+        setSearchTerm(e.target.value);
+        //Todo Perform search logic here
+        fetch('url' + searchTerm)
+            .then(response => response.json())
+            .then(data => {
+            
+            })
+            .catch(error => {
+            // Handle any errors
+            });
+    };
+
+    return (
+        <div className = 'searchBar'>
+            <input
+                type="text"
+                placeholder="Search..."
+                value={searchTerm}
+                //every change triggers a new query
+                onChange={handleSearch}
+            />
+        </div>
+    );
+};
+
+export default SearchBar;
--- Code/intercal/src/dashboard.jsx
+++ Code/intercal/src/dashboard.jsx
@@ -1,9 +1,17 @@
 import React, {useEffect, useState} from 'react';
 import './/styles/styles.css';
+<<<<<<< Updated upstream
 import './/styles/sidebarStyles.css'
 
 export default function Dashboard() {
   const [alerts, setAlerts] = useState([]);
+=======
+import './/styles/dashboard.css'
+
+
+export default function Dashboard() {
+  const [watched, setWatched] = useState([]);
+>>>>>>> Stashed changes
 
   //get data from the server
   /*
@@ -14,18 +22,37 @@ export default function Dashboard() {
       .catch(error => console.error(error));
   }, []);
   */
+<<<<<<< Updated upstream
   function addAlert(newAl){
     setAlerts(newAl =>{
       console.log(newAl);
       let newAr = [...alerts, newAl,];
+=======
+  // this function is purely test atm, as the actual favorite button is going to be built into the 
+  function addWatched(newWatch){
+    setWatched(newWatch =>{
+      console.log(newWatch);
+      let newAr = [...watched, newWatch,];
+>>>>>>> Stashed changes
       return newAr;
     });
   }
 
+<<<<<<< Updated upstream
     let as = alerts.map((fav) =>{
       return <a class = 'favorite' href = '{}'>{fav.id}</a>
     });
   return ( );
+=======
+    let as = watched.map((fav) =>{
+      return <a class = 'favorite' href = '{}'>{fav.id}</a>
+    });
+  return (
+  <div className='dashboard'>
+    {}
+  </div> 
+  );
+>>>>>>> Stashed changes
 }
 
 
