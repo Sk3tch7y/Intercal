f60ad8d976a7a3abf015b530f1bb4a45b4504907
Nicolaas <71842732+Nickouwen@users.noreply.github.com>
2024-03-18 18:15:03 -0700

    added test.js, changed package.json
    Changed package.json to try and automate testing

---
+++ Code/server/test.js
@@ -0,0 +1,9 @@
+const assert = require("assert");
+
+describe('Testing function...', function() {
+    it('2 = 2', function(done) {
+        let two = 2;
+        assert.equal(two, 2);
+        done();
+    })
+})
