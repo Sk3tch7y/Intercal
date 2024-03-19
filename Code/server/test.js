d669338fdb38b3a629d564cfcfea2ced1456723e
Nicolaas <71842732+Nickouwen@users.noreply.github.com>
2024-03-18 22:58:16 -0700

    Moved files

--- Code/server/test.js
+++
@@ -1,9 +0,0 @@
-const assert = require("assert");
-
-describe('Testing function...', function() {
-    it('2 = 2', function(done) {
-        let two = 2;
-        assert.equal(two, 2);
-        done();
-    })
-})
