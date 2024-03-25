e8d1b130714b6014c366bb1554272bb08a754122
OakleyPankratz <oakleyapankratz@gmail.com>
2024-03-25 11:50:08 -0700

    Five Number Summary Button
    Added a Button to Display the Five Number Summary for an array of numeric data. Currently displaying constants, still need to parse data from back end.

--- Code/intercal/src/GraphWrapper.jsx
+++ Code/intercal/src/GraphWrapper.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Graph from './Graph.jsx';
 
 class GraphWrapper extends React.Component {
     constructor(props) {
@@ -14,19 +15,20 @@ class GraphWrapper extends React.Component {
         // Fetch graph data and other data from an API or any other source
         // Update the state with the fetched data
         this.setState({
-            graphData: [/* graph data */],
+            graphData: [1, 2, 3, 4],
             otherData: 'Some other data',
         });
     }
 
     render() {
         const { graphData, otherData } = this.state;
-        //TODO: When Oakley IS done <Graph data={graphData} />
+        <Graph data={graphData} />
         return (
             <div>
                 <h1>Monitoring Post</h1>
                 <div className='sidemenu'>{otherData}</div>
                 <div className = 'graph'></div>
+                <Graph data = {graphData}></Graph>
             </div>
         );
     }
