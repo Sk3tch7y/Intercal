6a505a3888d493470df56299099eb48713bc9d25
Nicolaas <71842732+Nickouwen@users.noreply.github.com>
2024-03-25 22:49:47 -0700

    Uncommented a require command

--- Code/server/server.js
+++ Code/server/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
-//const { get } = require("request");
+const { get } = require("request");
 const stations = require('./queries');
 
 const app = express();
