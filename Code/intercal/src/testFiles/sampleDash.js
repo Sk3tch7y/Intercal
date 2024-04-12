3e91e1b7d12d5a30d5dc3866ed272d4a74358c8e
Adam Badry <57472142+Sk3tch7y@users.noreply.github.com>
2024-04-12 13:42:02 -0700

    Update sampleDash.js

--- Code/intercal/src/testFiles/sampleDash.js
+++ Code/intercal/src/testFiles/sampleDash.js
@@ -1,2 +1,7 @@
-export const sampleDash = () => fetch("http://localhost:8080/search?searchTerm=")
-  .then(response => response.json());
+const sampleDash = () => fetch("http://localhost:8080/search?searchTerm=")
+  .then(response => response.json().then(data => {
+    console.log(data);
+    return data;
+  }));
+
+ export { sampleDash };
